4689a1ef9938a1ab60d1ecc5b26b72d00bbfb700
Luke Craig <lacraig3@gmail.com>
2020-04-14 16:18:27 -0400

    first set of files. updates to things

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t kernelinfobuilder . --no-cache
+docker build -t kernelinfobuilder .
--- downloader.py
+++ downloader.py
@@ -1,32 +1,40 @@
+import apt
+from requests import get
 from subprocess import check_output
 from shutil import rmtree,move
 from os.path import exists,basename
 from os import listdir, remove, chdir
 from glob import iglob
 
+cache = apt.cache.Cache()
+cache.update()
+cache.open()
+
 rmtree("/workspace/downloads",ignore_errors=True)
 
-lines = check_output(["apt-cache", "search", "linux-image-unsigned-*"]).decode().split("\n")
-for line in lines:
-	if line:
-		package_name = line.split(" - ")[0]
-		used = False
-		for df in listdir("/workspace/output/"):
-			if package_name in df:
-				used = True
-		if "dbgsym" in package_name and not used:
-			print(f"Downloading package: {package_name}")
-			print(check_output(["apt-get", "-y", "install", "--download-only", package_name]))
-			print(check_output(["mkdir","/workspace/downloads"]))
-			download_file = [df for df in listdir("/var/cache/apt/archives/") if "linux-image-unsigned" in df][0]
-			move(f"/var/cache/apt/archives/{download_file}",f"/workspace/downloads/{basename(download_file)}")
-			chdir("./downloads")
-			print(check_output(["ar", "xv", f"/workspace/downloads/{basename(download_file)}"]))
-			chdir("..")
-			print(check_output(["tar", "-xf", "/workspace/downloads/data.tar.xz"]))
-			vmlinuxfile = [f for f in iglob("**/vmlinux*",recursive=True)][0]
-			move(vmlinuxfile,"./vmlinux")
+package_list = [key for key in cache.keys() if "linux-image-unsigned-" in key]
+
+for package_name in package_list:
+	if not exists(f"/workpace/output/{package_name}_kernelinfo.conf"):
+		print(check_output(["mkdir","/workspace/downloads"]))
+		print(f"Downloading package: {package_name}")
+		package = cache[package_name]
+		with open(f"/workspace/downloads/{package_name}", "wb") as f:
+			response = get(package.candidate.uri)
+			f.write(response.content)
+		chdir("/workspace/downloads")
+		print(check_output(["ar", "xv", f"/workspace/downloads/{package_name}"]))
+		chdir("/workspace")
+		print(check_output(["tar", "-xvf", "/workspace/downloads/data.tar.xz"]))
+		vmlinuxfiles = [f for f in iglob("**/vmlinux*",recursive=True)]
+		if len(vmlinuxfiles):
+			vmlinuxfile = vmlinuxfiles[0]
+			move(vmlinuxfile,"/workspace/vmlinux")
 			print(check_output(["./run.sh", "vmlinux", f"./output/{package_name}_kernelinfo.conf"]))
-			remove("./vmlinux")
-			rmtree("/workspace/downloads",ignore_errors=True)
+			remove("/workspace/vmlinux")
+		else:
+			print("couldn't find a vmlinux file for {package_name}")
+		rmtree("/workspace/downloads",ignore_errors=True)
+	else:
+		print(f"Continued on from {package_name}")
 
